a34f7f32da22e0d4d6c6ef146913a1baee6237f0
CharlotteNiya <116511789+CharlotteNiya@users.noreply.github.com>
2021-02-27 07:44:55 +0000

    Run update-remote.sh on aerospike

--- app/src/main/java/com/diprana/riskcalculator/PortoDiffUtil.kt
+++ app/src/main/java/com/diprana/riskcalculator/PortoDiffUtil.kt
@@ -2,4 +2,6 @@ package com.diprana.riskcalculator
 
 import androidx.recyclerview.widget.DiffUtil.ItemCallback
 
-class PortoDiffUtil : ItemCallback<RiskPor
+class PortoDiffUtil : ItemCallback<RiskPorto>() {
+
+  override fun areIte
