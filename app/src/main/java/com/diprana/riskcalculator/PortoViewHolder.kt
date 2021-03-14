e633e5addad4fec9a2f5d35705a07f68a6b34a18
CharlotteNiya <116511789+CharlotteNiya@users.noreply.github.com>
2021-03-14 22:35:52 +0000

    theme changes

--- app/src/main/java/com/diprana/riskcalculator/PortoViewHolder.kt
+++ app/src/main/java/com/diprana/riskcalculator/PortoViewHolder.kt
@@ -3,4 +3,6 @@ package com.diprana.riskcalculator
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import kotlinx.android.synthetic.main.item_risk_porto.view.risk_porto_percentage
-import kotlinx.android.synthetic.main.item_risk_porto.vi
+import kotlinx.android.synthetic.main.item_risk_porto.view.risk_to_porto
+
+class PortoView
