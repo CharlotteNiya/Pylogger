b7c5a64d48b237019e9a0e1b20ac7fca9197c154
CharlotteNiya <116511789+CharlotteNiya@users.noreply.github.com>
2021-03-14 18:17:36 +0000

    kmemleak: allow freeing internal objects after kmemleak was disabled

--- app/src/main/java/com/diprana/riskcalculator/PortoViewHolder.kt
+++ app/src/main/java/com/diprana/riskcalculator/PortoViewHolder.kt
@@ -2,4 +2,5 @@ package com.diprana.riskcalculator
 
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
-import kotlinx.android.synthetic.main.item_risk_porto.v
+import kotlinx.android.synthetic.main.item_risk_porto.view.risk_porto_percentage
+import kotlinx.andr
