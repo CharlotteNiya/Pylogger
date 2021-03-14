ce3ceed7435e1505688028e2bf0441b4bf09ed00
CharlotteNiya <116511789+CharlotteNiya@users.noreply.github.com>
2021-03-14 16:37:11 +0000

    Clean up logging.

--- app/src/main/java/com/diprana/riskcalculator/PortoViewHolder.kt
+++ app/src/main/java/com/diprana/riskcalculator/PortoViewHolder.kt
@@ -2,4 +2,4 @@ package com.diprana.riskcalculator
 
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
-import kotlinx.android.synt
+import kotlinx.android.synthetic.main.item_risk_porto.v
