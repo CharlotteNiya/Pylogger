930455f1e80281274b557b496830cbda06fcce19
CharlotteNiya <116511789+CharlotteNiya@users.noreply.github.com>
2021-03-14 22:27:28 +0000

    add LICENSE and README

--- app/src/main/java/com/diprana/riskcalculator/PortoViewHolder.kt
+++ app/src/main/java/com/diprana/riskcalculator/PortoViewHolder.kt
@@ -3,4 +3,4 @@ package com.diprana.riskcalculator
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import kotlinx.android.synthetic.main.item_risk_porto.view.risk_porto_percentage
-import kotlinx.andr
+import kotlinx.android.synthetic.main.item_risk_porto.vi
