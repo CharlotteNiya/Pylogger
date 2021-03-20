4f025396d82d107dc3d350388d8019609e94483d
CharlotteNiya <116511789+CharlotteNiya@users.noreply.github.com>
2021-03-20 08:49:58 +0000

    uprobes, x86: Fix _TIF_UPROBE vs _TIF_NOTIFY_RESUME

--- app/src/main/java/com/diprana/riskcalculator/PortoViewHolder.kt
+++ app/src/main/java/com/diprana/riskcalculator/PortoViewHolder.kt
@@ -5,4 +5,5 @@ import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import kotlinx.android.synthetic.main.item_risk_porto.view.risk_porto_percentage
 import kotlinx.android.synthetic.main.item_risk_porto.view.risk_to_porto
 
-class PortoViewHolder(itemView: View) : ViewHolder
+class PortoViewHolder(itemView: View) : ViewHolder(itemView) {
+  fun bind(item: RiskPo
