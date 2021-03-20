47822c13fbbfd28580e0fe2ce2c0b56437b1e392
CharlotteNiya <116511789+CharlotteNiya@users.noreply.github.com>
2021-03-20 11:55:04 +0000

    Update wsgi.py

--- app/src/main/java/com/diprana/riskcalculator/PortoViewHolder.kt
+++ app/src/main/java/com/diprana/riskcalculator/PortoViewHolder.kt
@@ -7,4 +7,6 @@ import kotlinx.android.synthetic.main.item_risk_porto.view.risk_to_porto
 
 class PortoViewHolder(itemView: View) : ViewHolder(itemView) {
   fun bind(item: RiskPorto) {
-    val riskPercentage = String.
+    val riskPercentage = String.format("%.2f", item.risk)
+
+    itemView.risk_to_p
